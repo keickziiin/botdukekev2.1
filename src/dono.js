8b885bcc0385f428e541f246db3af801d77351d4
keickziiin <64296785+keickziiin@users.noreply.github.com>
2021-01-10 17:26:57 -0300

    Add files via upload

---
+++ src/dono.js
@@ -0,0 +1,38 @@
+const menudono = (prefix) => {
+	return `
+
+╔════════════════
+  ᏟϴᎷᎪΝᎠϴՏ Ꭰϴ ᎠϴΝϴ
+╚════════════════
+
+
+╭────────────╮
+
+  *${prefix}loli*
+  *${prefix}hentai*
+  *${prefix}dono*
+  *${prefix}porno*
+ *${prefix}boanoite*
+  *${prefix}bomdia*
+  *${prefix}boatarde*
+  *${prefix}mia*
+  *${prefix}mia1*
+  *${prefix}mia2*
+  *${prefix}belle*
+  *${prefix}belle1*
+  *${prefix}belle2*
+  *${prefix}belle3*
+  *${prefix}ayeko*
+
+╰────────────╯`
+}
+
+exports.menudono = menudono
+
+
+
+
+
+
+
+
---
+++ src/help.js
@@ -0,0 +1,243 @@
+const help = (prefix) => {
+	return `
+
+<═══════════════════════>
+           ᏴϴͲ ᎠႮ ᏦᎬᏦᎬ ᏙᎬᎡՏᎪϴ 2
+ <═══════════════════════>
+
+             ╲    ╱    ●ᏴϴͲ2●ᎷᎬΝႮ●
+       ╱▔▔▔▔▔╲       Author    : ՏᎡ.ᏦᎬᏦᎬ
+      ┃┈▇┈┈▇┈┃      
+╭╮┣━━━━━━┫╭╮    
+┃┃┃┈┈┈┈┈┈┃┃┃    
+╰╯┃┈┈┈┈┈┈┃╰╯
+      ╰┓┏━━┓┏╯
+         ╰╯      ╰╯
+
+ ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+ Sigla atual: ${prefix} versão do bot: 2.0
+ ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+╔═════════════════════════╗
+ ✇─────────✇MENU✇─────────✇
+╚═════════════════════════╝
+
+╔══════════════════════
+║ᎬХᏟᏞႮՏᏆᏙϴ ᏢᎪᎡᎪ ᎪᎠᎷ ϴႮ 
+║ᏟᎡᏆᎪᎠϴᎡ
+╠══════════════════════
+║╭─────────────────────
+║├─*${prefix}grup abrir/fecha*
+║├─*${prefix}group abrir/fecha*
+║├─*${prefix}grupo abrir/fecha*
+║├────> fecha/abrir grupo
+║├─────────────────────
+║├─*${prefix}setpp mnd foto cm legenda*
+║├─*${prefix}fotogp mnd foto cm legenda*
+║├────> muda foto do grupo
+║├─────────────────────
+║├─*${prefix}welcome 0/1*
+║├─*${prefix}bv 0/1*
+║├────> boas vindas
+║├─────────────────────
+║├─*${prefix}leaver*
+║├────> bot sair do grupo
+║├─────────────────────
+║├─*${prefix}linkgroup*
+║├────> link do grupo
+║├─────────────────────
+║├─*${prefix}listadmin*
+║├────> lista de ADMs
+║├─────────────────────
+║├─*${prefix}ban @membro*
+║├─*${prefix}remover @membro*
+║├────> banir membro
+║├─────────────────────
+║├─*${prefix}add 55xxx*
+║├─*${prefix}adicionar 55xxx*
+║├────> adicionar menbro
+║├─────────────────────
+║├─*${prefix}promote @membro*
+║├─*${prefix}promover @membro*
+║├────> promover a ADM
+║├─────────────────────
+║├─*${prefix}demote @adm*
+║├─*${prefix}rebaixar @adm*
+║├────> tirar adm
+║├─────────────────────
+║├─*${prefix}marcar1*
+║├────> marca TDS membros
+║├─────────────────────
+║├─*${prefix}marcar2*
+║├────> marca TDS membros
+║├─────────────────────
+║├─*${prefix}marcar3*
+║├────> marca TDS membros
+║├─────────────────────
+║├─*${prefix}ts*
+║├────> fazer TM
+║├─────────────────────
+║├─*${prefix}limpar*
+║├────> limpa msgs
+║├─────────────────────
+║├─*${prefix}setprefix*
+║├────> mudar sigla de comandos
+║├─────────────────────
+║├─*${prefix}listadebloq*
+║├────> números bloqueados
+║└─────────────────────
+╠═══════════════════════
+╠══════════════════════
+║      ᏢᎬՏϘႮᏆՏᎪᎡ/ႮᎡᏞ/ᏞᏆΝᏦ
+╠══════════════════════
+║╭─────────────────────
+║├─*${prefix}url2img*
+║├────> tira print do site
+║├─────────────────────
+║├>*${prefix}tiktokstalk nome*
+║├────> pega informações da conta
+║├─────────────────────
+║├─*${prefix}tiktok*
+║├────>[?]
+║├─────────────────────
+║├─*${prefix}yt2mp3 seu link*
+║├─*${prefix}ytbaixa seu link*
+║├────> baixa video como msc
+║├─────────────────────
+║├─*${prefix}ytsearch título*
+║├─*${prefix}ytpesquisar título*
+║├────> pesquisa vídeo de yt
+║└─────────────────────
+╠══════════════════════
+╠══════════════════════
+║      ҒᏆᏀ/ᎷᏆᎠᏆᎪ/+18
+╠══════════════════════
+║╭─────────────────────
+║├─*${prefix}stiker*
+║├─*${prefix}sticker*
+║├─*${prefix}fig*
+║├────> fazer fig cm foto/gif
+║├─────────────────────
+║├─*${prefix}toimg*
+║├─*${prefix}converter*
+║├────> converter fig em foto
+║├─────────────────────
+║├─*${prefix}wait*
+║├────> pesquisa anime de imagem
+║├─────────────────────
+║├─*${prefix}tsticker*
+║├─${prefix}tstiker*
+║├────> fazer fig cm txt
+║├─────────────────────
+║├─*${prefix}memeindo*
+║├────> meme [indonésio]
+║├─────────────────────
+║├─*${prefix}meme*
+║├────> meme [inglês]
+║├─────────────────────
+║├─*${prefix}reislin*
+║├────>[?]
+║├─────────────────────
+║├─*${prefix}tutorial*
+║├────> aprender o básico de bot
+║├─────────────────────
+║├─*${prefix}dono*
+║├────> ...
+║├─────────────────────
+║├─*${prefix}ocr*
+║├────> pega txt de uma imagem
+║├─────────────────────
+║├─*${prefix}audio pt seu txt*
+║├─*${prefix}gtts pt seu txt*
+║├────> converter txt em áudio
+║├─────────────────────
+║├─*${prefix}info*
+║├────> info sobre o bot
+║├─────────────────────
+║├─*${prefix}tulis*
+║├─*${prefix}nulis*
+║├────> [?]
+║├─────────────────────
+║├─*${prefix}clone @membro*
+║├────> [?]
+║├─────────────────────
+║├─*${prefix}bomdia*
+║├────> foto pra alegrar o dia
+║├─────────────────────
+║├─*${prefix}boatarde*
+║├────> foto para alugar sua tarde
+║├─────────────────────
+║├─*${prefix}boanoite*
+║├────> foto para alegrar sua noite
+║├─────────────────────
+║├─*${prefix}map 0000-0000*
+║├─*${prefix}mapa 0000-000*
+║├────> pega imagem através do cód
+║├─────────────────────
+║├─*${prefix}text3d seu txt*
+║├─*${prefix}texto3d seu txt*
+║├────> converte txt em foto 3d
+║├─────────────────────
+║├─*${prefix}anjing*
+║├─*${prefix}auau*
+║├─*${prefix}dog*
+║├────> foto de dog fofo
+║├─────────────────────
+║├─*${prefix}phlogo seu|txt*
+║├────> logo da ph cm seu txt
+║├─────────────────────
+║├─*${prefix}criador*
+║├─*${prefix}adm*
+║├────> bot irá manda o criador
+║├─────────────────────
+║├─*${prefix}lofi*
+║├────> lofizinho
+║├─────────────────────
+║├─*${prefix}nsfwloli*
+║├────> lolizinha
+║├─────────────────────
+║├ᏟϴᎷᎪΝᎠϴ Ꭺ ᏴᎪᏆХϴ +18
+║├─────────────────────
+║├─*${prefix}porno*
+║├────> >-<
+║├─────────────────────
+║├─*${prefix}mia1*
+║├────> use para vê
+║├─────────────────────
+║├─*${prefix}mia2*
+║├────> use para vê
+║├─────────────────────
+║├─*${prefix}mia3*
+║├────> use para vê
+║├─────────────────────
+║├─*${prefix}belle1*
+║├────> use para vê
+║├─────────────────────
+║├─*${prefix}belle2*
+║├────> use para vê
+║├─────────────────────
+║├─*${prefix}belle3*
+║├────> use para vê
+║├─────────────────────
+║├─*${prefix}belle4*
+║├────> use para vê
+║├─────────────────────
+║├─*${prefix}hentai*
+║├────> hentaizin
+║├─────────────────────
+║├─*${prefix}malkova*
+║├────> [?]
+║├─────────────────────
+║├─*${prefix}akeno*
+║├────> use para vê
+║└─────────────────────
+╚═══════════════════════
+ՏᎡ.ᏦᎬᏦᎬ ͲᎻᎬ ᏴᎬՏͲ❦ᵃᵗᵉⁿᵃ© 2021`
+}
+
+exports.help = help
+
+
+
+
+
+
